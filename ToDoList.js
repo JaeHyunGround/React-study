afec70eec7f400319b3f382dfe3dbe151618ec57
JaeHyunGround <97944429+JaeHyunGround@users.noreply.github.com>
2022-02-03 01:21:05 +0900

    React 학습 : To Do List

--- ToDoList.js
+++ ToDoList.js
@@ -1,26 +1,36 @@
-import {useState, useEffect} from "react";
-
+import { useState } from "react";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json); 
-      setLoading(false);
-    });
-  }, []);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
+  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
       <ul>
-        {coins.map((coin) => <li>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}</li>)};
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
       </ul>
     </div>
   );
-
-  }
-
-export default App;
+}
